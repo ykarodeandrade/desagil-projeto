203c427259383111d72759359fa26e8c64cbd05d
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-11-16 06:09:30 -0300

    Add rounded wrapper

--- Frontend/lib/components/index.js
+++ Frontend/lib/components/index.js
@@ -1,9 +1,10 @@
 // NÃO MODIFIQUE NEM SUBSTITUA ESTE ARQUIVO
 
+import Rounded from './Rounded';
 import AspectView from './AspectView';
 import Icon from './Icon';
 import DropDown from './DropDown';
 import DateTimePicker from './DateTimePicker';
 import DateTimeProvider from './DateTimeProvider';
 
-export { AspectView, Icon, DropDown, DateTimePicker, DateTimeProvider };
+export { Rounded, AspectView, Icon, DropDown, DateTimePicker, DateTimeProvider };
