6823732bf804de412ff9b98329e931d69e9c9c2e
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-11-22 08:21:54 -0300

    Add example

--- Frontend/components/Main.js
+++ Frontend/components/Main.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View } from 'react-native';
 
-import { Text } from 'react-native-paper';
+import { ActivityIndicator, Text, Button, Snackbar } from 'react-native-paper';
 
-import { StatusBar } from 'expo-status-bar';
+import { useGPS } from '../lib';
 
 import styles from '../styles/Main.json';
 
 export default function Main(props) {
+    const [error, setError] = useState(false);
+
+    const { gps } = useGPS(null);
+
+    function onPress() {
+        setError(true);
+        gps.update();
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Abra Main.js para começar a trabalhar no seu app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <>
+            <View style={styles.container}>
+                {gps.updating ? (
+                    <ActivityIndicator style={styles.input} size="small" />
+                ) : (
+                    gps.location && (
+                        <Text style={styles.input}>
+                            {JSON.stringify(gps.location, null, 2)}
+                        </Text>
+                    )
+                )}
+                <Button style={styles.input} onPress={onPress}>
+                    Atualizar
+                </Button>
+            </View>
+            {!gps.updating && gps.broken && (
+                <Snackbar visible={error} action={{ label: 'Ok', onPress: () => setError(false) }} onDismiss={() => { }}>
+                    {gps.allowed ? 'Não foi possível atualizar a localização.' : 'Não foi possível acessar o GPS.'}
+                </Snackbar>
+            )}
+        </>
     );
 }
