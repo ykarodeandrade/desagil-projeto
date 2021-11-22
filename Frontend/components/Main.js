3b8405a15bb98beedb2d13fc18268793179148af
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-11-22 08:02:51 -0300

    Add example

--- Frontend/components/Main.js
+++ Frontend/components/Main.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View } from 'react-native';
 
-import { Text } from 'react-native-paper';
+import { Portal, Text, Button, Snackbar } from 'react-native-paper';
 
-import { StatusBar } from 'expo-status-bar';
+import { useScanner } from '../lib';
 
 import styles from '../styles/Main.json';
 
 export default function Main(props) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
+
+    const { scanner, Preview } = useScanner();
+
+    function onBarCodeScanned(result) {
+        setData(result.data);
+        scanner.deactivate();
+    }
+
+    function onPress() {
+        setError(true);
+        scanner.activate();
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Abra Main.js para começar a trabalhar no seu app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <>
+            {scanner.active ? (
+                <Portal>
+                    <Preview style={styles.preview} onBarCodeScanned={onBarCodeScanned} />
+                </Portal>
+            ) : (
+                <View style={styles.container}>
+                    {data && (
+                        <Text style={styles.input}>
+                            {data}
+                        </Text>
+                    )}
+                    <Button style={styles.input} onPress={onPress}>
+                        Escanear
+                    </Button>
+                </View>
+            )}
+            {scanner.broken && (
+                <Snackbar visible={error} action={{ label: 'Ok', onPress: () => setError(false) }} onDismiss={() => { }}>
+                    Não foi possível acessar a câmera.
+                </Snackbar>
+            )}
+        </>
     );
 }
